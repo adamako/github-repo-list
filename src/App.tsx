9d09c82d774ceb1c7a46dc474a64ae3beda22b8f
Adama KO <adamako34@hotmail.com>
2023-07-16 22:49:12 +0000

    refactor: remove debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { API_URL } from "./config";
 import { Header, RepositoryList, SearchInput } from "./components/molecules";
-import debounce from "./utils/debounce";
 import { Repository } from "./types";
 
 function App() {
@@ -22,19 +21,29 @@ function App() {
     }
   };
 
-  const debouncedSearch = debounce(fetchRepositories, 1000);
-
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
-    debouncedSearch(event.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await fetchRepositories(inputValue);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="container mx-auto px-20">
       <Header />
       <div className="w-full bg-white mt-10 p-10 rounded rounded-[10px] h-full">
-        <h1 className="text-2xl">Welcome</h1>
-        <SearchInput inputValue={inputValue} handleChange={handleChange} />
+        <h1 className="text-2xl text-center mb-5">Welcome</h1>
+        <SearchInput
+          inputValue={inputValue}
+          handleChange={handleChange}
+          handelSubmit={handleSubmit}
+        />
         <div className="mt-5">
           {repositories.length > 0 && (
             <h1 className="text-xl text-left">Repositories</h1>
